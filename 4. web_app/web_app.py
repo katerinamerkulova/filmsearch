1c02bd179a341d8d83f0342bcaf0d4d21e2feb69
katerinamerkulova <merkulova1999@mail.ru>
2020-08-06 16:59:33 +0300

    initial coomit
    add notebooks and files

---
+++ 4. web_app/web_app.py
@@ -0,0 +1,23 @@
+__author__ = 'xead'
+from codecs import open
+from flask import Flask, render_template, request
+import filmsearch
+
+app = Flask(__name__)
+@app.route("/", methods=["POST", "GET"])
+def index_page(text="", prediction_message=""):
+    if request.method == "POST":
+        text = request.form.get("text")
+        logfile = open("ydf_demo_logs.txt", "a", "utf-8")
+        prediction_message = filmsearch.predict(text)
+        print("<response>", file=logfile)
+        print(text, file=logfile)
+        print(prediction_message, file=logfile)
+        print("</response>", file=logfile)
+        logfile.close()
+    return render_template('filmsearch.html', text=text, prediction_message=prediction_message)
+
+
+if __name__ == "__main__":
+    app.run()
+
