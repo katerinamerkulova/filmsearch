01ac01c8d7d8d861faac887f7ff04e7be96652fc
katerinamerkulova <merkulova1999@mail.ru>
2020-12-06 12:33:43 +0300

    fix parsing module

---
+++ 1. crawling & parsing/main.py
@@ -0,0 +1,48 @@
+import requests
+import bs4
+import re
+import wikipediaapi as wapi
+wiki = wapi.Wikipedia('ru')
+
+
+def crawl_linkspage(url, common, length, film_titles=[]):
+    page = requests.get(url)
+    parser = bs4.BeautifulSoup(page.text, 'lxml')
+    links = parser.find_all('li')[2:205]
+
+    for link in links[:-3]:
+        title = link.a.get('title')
+        if not re.match('Категория:', title):
+            film_titles.append(title)
+
+    if len(film_titles) < length:
+        url = common + re.search('pagefrom.+pages', str(parser)).group(0)    # next page
+        try:
+            crawl_linkspage(url, common, length, film_titles)
+        except AttributeError:
+            return film_titles 
+    return film_titles
+
+
+def parsing(titles):
+    for title in titles:
+        page = wiki.page(title)
+        
+        for sect in page.sections:
+            if sect.title in ('Сюжет', 'Синопсис'):
+                plot = sect.text    # plot
+                summary = page.summary
+
+                with open('film_plots.txt', 'a', encoding='utf-8') as f:
+                    f.write(plot + '\n&&&\n' )
+                with open('summary.txt', 'a', encoding='utf-8') as f:
+                    f.write(summary + '\n&&&\n' )
+                break
+        
+
+
+def tokenization(texts):
+    tokenized_texts = [' '.join(re.findall('\w+', text.lower())) for text in texts]
+    with open('tokenized_film_plots.txt', 'w', encoding='utf-8') as f:
+        f.write('\n&&&\n'.join(tokenized_texts))
+
